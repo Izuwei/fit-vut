28412eb2011e8d592be179b30ff8d3f442dc61a2
Izuwei <37940354+Izuwei@users.noreply.github.com>
2021-04-04 23:12:25 +0200

    Add files via upload

--- 2MIT/FLP/functional/plg-2-nka.hs
+++ 2MIT/FLP/functional/plg-2-nka.hs
@@ -1,3 +1,7 @@
+-- FIT VUT
+-- Author: Jakub Sadilek
+-- Login: xsadil07
+
 import System.Environment
 import System.IO
 import Data.List
@@ -31,7 +35,6 @@ data FSM = FSM { states :: [State]              -- Q
                , initState :: State             -- q0
                , finalStates :: [State]         -- F
                } deriving (Show, Eq)
-
 
 -- Function returns pair (index, filename) according to program parameters.
 parseArgs :: Num a => [[Char]] -> (a, [Char])
@@ -68,41 +71,55 @@ loadGrammar (nonterms : terms : initNonterm : rules) =
     where
         getSymbols line = split ',' line
 
+        -- Function checks if given symbol is in set of nonterminals.
         checkNonterm symbol
             | symbol `elem` getSymbols nonterms = symbol
             | otherwise = error ("Symbol '" ++ symbol ++ "' is not nonterminal.")
 
+        -- Function get lines from input and parse them into rule struct (N, [N u T]).
         getRules lines = [splitRule line | line <- lines]
 
+        -- Function splits rule on arrow (->) and returns both sides in pair.
         splitRule rule@(x:y:z:xs)
             | [y, z] == "->" = ([x], map (: []) xs)
             | otherwise = error ("Unknown rule format: " ++ rule)
 
         splitRule rule = error ("Unknown rule format: " ++ rule)
 
-        checkNonterms symbols = [map isUppercase symbol | symbol <- symbols]
+        -- Function checks if symbol is not empty. If is empty - error is raised.
+        isNotEmpty symbol
+            | null symbol = error "Empty symbols are not allowed."
+            | otherwise = symbol
+
+        -- Function checks if list of symbols is full of nonterms according to assignment.
+        checkNonterms symbols = [map isUppercase (isNotEmpty symbol) | symbol <- symbols]
             where
                 isUppercase symbol
                     | symbol `elem` ['A' .. 'Z'] = symbol
                     | otherwise = error ("Nonterminal '" ++ [symbol] ++ "' must be in range A-Z.")
 
-        checkTerms symbols = [map isLowercase symbol | symbol <- symbols]
+        -- Function checks if list of symbols is full of terms according to assignment.
+        checkTerms symbols = [map isLowercase (isNotEmpty symbol) | symbol <- symbols]
             where
                 isLowercase symbol
                     | symbol `elem` ['a' .. 'z'] = symbol
                     | otherwise = error ("Terminal '" ++ [symbol] ++ "' must be in range a-z.")
 
+        -- Function checks if rules are in shape of right linear grammar.
         checkRules rls = [(checkNonterm (fst rule), checkRight (snd rule)) | rule <- rls]
             where
-                isAllTerm [] = True
+                isAllTerm [] = True     -- Function checks if list of strings is full of terms, then returns bool.
                 isAllTerm [t] = True
                 isAllTerm (t:ts)
                     | t `notElem` getSymbols terms = False
                     | otherwise = isAllTerm ts
+                
+                -- Functions checks right side of the rule (xyz.., aB).
+                checkRight [] = error "Bad right side of rule."
                 checkRight nut =
                     if (last nut `elem` getSymbols nonterms && length nut /= 1)
                       || last nut `elem` getSymbols terms
-                      || (last nut == "#" && length nut == 1) then      -- Opravit že levy neterminal se nenachazi na zadne prave strane?
+                      || (last nut == "#" && length nut == 1) then
 
                         if isAllTerm nut then
                             nut
@@ -114,7 +131,7 @@ loadGrammar (nonterms : terms : initNonterm : rules) =
 
 loadGrammar _ = error "Insufficient input."
 
-
+-- Function prints grammar according the assignment.
 showGrammar :: Grammar -> IO ()
 showGrammar grammar = do
     putStrLn (intercalate "," (nonterms grammar))
@@ -122,11 +139,15 @@ showGrammar grammar = do
     putStrLn (initNonterm grammar)
     mapM_ (\rule -> putStrLn (fst rule ++ "->" ++ concat (snd rule))) (rules grammar)
 
-nontermGenerator :: (Foldable t, Show a, Num a) => a -> [Char] -> t [Char] -> [Char]
+-- Function gets starting number, nonterm and list of nonterms.
+-- Then functions returns a new nonterm that is not in given list of nonterms.
+nontermGenerator :: Integer -> [Char] -> [[Char]] -> [Char]
 nontermGenerator n nonterm nonterms
     | (head nonterm : show n) `elem` nonterms = nontermGenerator (n+1) nonterm nonterms
     | otherwise = head nonterm : show n
 
+-- Functions gets grammar rules and terms.
+-- Then returns all nonterms, which are included in rules.
 getNonterms :: [[Char]] -> [([Char], [[Char]])] -> [[Char]]
 getNonterms terms rules = sort $ removeDuplicities $ getNontermList terms rules
     where
@@ -137,92 +158,109 @@ getNonterms terms rules = sort $ removeDuplicities $ getNontermList terms rules
             | x `elem` xs = removeDuplicities xs
             | otherwise = x : removeDuplicities xs
 
+-- Function transforms grammar according TIN algorithm in sentence 3.2.
 transformGrammar :: Grammar -> Grammar
 transformGrammar grammar =
-    Grammar (getNonterms (terms grammar) transformedRules)
-            (terms grammar)
-            transformedRules
-            (initNonterm grammar)
+    Grammar (getNonterms (terms grammar) transformedRules)  -- Nonterms
+            (terms grammar)                                 -- Terms
+            transformedRules                                -- Rules
+            (initNonterm grammar)                           -- Initial nonterm
     where
+        -- New transformed rules
         transformedRules = transformRules (nonterms grammar) (rules grammar)
 
+        -- Transform long rule into sequence of short rules.
+        -- Creates a new rule and recursively do this for rest of the given rule.
         cascadeRule nonterms (l, r) =
             (newNonterms, (l, [head r, newNonterm]) : newRules)
             where
-                newNonterm = nontermGenerator 1 l nonterms
+                newNonterm = nontermGenerator 1 l nonterms -- newNonterm
                 (newNonterms, newRules) = transformRule (nonterms ++ [newNonterm]) (newNonterm, tail r)
 
+        -- Function transforms simple terminal rule like X->a into X->aX1, X1->eps.
+        -- Returned in pair (new Nonterms, new Rules).
         transformTerminalRule nonterms (l, r) =
             (nonterms ++ [newNonterm], [(l, [head r, newNonterm]), (newNonterm, ["#"])])
             where
                 newNonterm = nontermGenerator 1 l nonterms
 
+        -- Function transform a single rule.
+        -- Returns pair as (new Nonterms, new Rule).
         transformRule nonterms rule@(l, r)
-            | length r == 1 && last r == "#" = (nonterms, [rule])
-            | length r == 1 = transformTerminalRule nonterms rule -- For not eps rules use this: (nonterms, [rule])
-            | length r == 2 && last r `elem` nonterms = (nonterms, [rule])
-            | otherwise = cascadeRule nonterms rule
-            -- comment: | length r > 2 && last r `elem` nonterms = cascadeRule nonterms rule
+            | length r == 1 && last r == "#" = (nonterms, [rule])   -- Terminal rule X->#
+            | length r == 1 = transformTerminalRule nonterms rule   -- Simple rule X->a. For not eps rules use this: (nonterms, [rule])
+            | length r == 2 && last r `elem` nonterms = (nonterms, [rule])  -- X->aB
+            | otherwise = cascadeRule nonterms rule     -- Other long rules like X->aaaa, X->aaaaB
 
+        -- Function gets list of rules and transform them, then new rules are returned.
+        transformRules _ [] = error "There is no rules."
         transformRules nonterms [rule] = snd $ transformRule nonterms rule
         transformRules nonterms (rule:rules) =  newRules ++ transformRules newNonterms rules
             where
                 (newNonterms, newRules) = transformRule nonterms rule
 
+-- Functions covenrts grammar into FSM.
 loadFSM :: Grammar -> FSM
 loadFSM grammar =
-    FSM ([i | (_, i) <- convTable])
-        (terms grammar)
-        (getTransitions (rules grammar))
-        (getState (initNonterm grammar) convTable)
-        (getFinalStates (rules grammar))
+    FSM ([i | (_, i) <- convTable])                 -- states
+        (terms grammar)                             -- alphabet
+        (getTransitions (rules grammar))            -- transitions
+        (getState (initNonterm grammar) convTable)  -- initial state
+        (getFinalStates (rules grammar))            -- list of terminal states
     where
-        convTable = nontermsToStates (nonterms grammar)
+        convTable = nontermsToStates (nonterms grammar) -- Map nonterms into states
 
+        -- Function gets list of nonterms and each nonterm is associated with number.
+        -- Numbers starts from 1 and increases with each nonterm. Retured as list of pairs.
         nontermsToStates = numberStates 1
             where
                 numberStates _ [] = []
                 numberStates n (x:xs) = (x, n) : numberStates (n+1) xs
 
+        -- Function gets nonterm and convTable and returns state, which corresponds to given nonterm.
         getState nonterm [] = error ("Unknown Nonterm: " ++ nonterm)
         getState nonterm ((n, i):xs)
             | nonterm == n = i
             | otherwise = getState nonterm xs
 
+        -- Function convert single rule into transition.
         getTransition (l, r) = Transition (getState l convTable)
                                           (head r)
                                           (getState (last r) convTable)
 
+        -- Function converts list of rules into list of transitions.
         getTransitions [] = []
         getTransitions (rule:rules)
-            | length (snd rule) == 1 = getTransitions rules
+            | length (snd rule) == 1 = getTransitions rules   -- skip terminal rules X->#
             | otherwise = getTransition rule : getTransitions rules
 
+        -- Function returns all terminal states from grammar rules.
         getFinalStates rules = map (\(a, _) -> getState a convTable) (filter (\(_, y) -> last y == "#") rules)
 
+-- Function prints FSM according the assignment.
 showFSM :: FSM -> IO ()
 showFSM fsm = do
-    putStrLn (intercalate "," [show state | state <- states fsm])
-    putStrLn (concat (alphabet fsm))
-    print (initState fsm)
-    putStrLn (intercalate "," [show state | state <- finalStates fsm])
-    mapM_ putStrLn $ sort [intercalate "," [show (state t), symbol t, show (nextState t)] | t <- transitions fsm]
+    putStrLn (intercalate "," [show state | state <- states fsm])       -- states
+    putStrLn (concat (alphabet fsm))                                    -- alphabet
+    print (initState fsm)                                               -- initial state
+    putStrLn (intercalate "," [show state | state <- finalStates fsm])  -- final states
+    mapM_ putStrLn $ sort [intercalate "," [show (state t), symbol t, show (nextState t)] | t <- transitions fsm]   -- transitions
 
 main :: IO ()
 main = do
     args <- getArgs
 
     let (option, file) = parseArgs args
-    content <- lines <$> loadInput file
+    content <- fmap lines $ loadInput file
 
     let grammar = loadGrammar content
 
     case option of
-        0 -> showGrammar grammar
-        1 -> showGrammar $ transformGrammar grammar
-        2 -> showFSM $ loadFSM $ transformGrammar grammar
+        0 -> showGrammar grammar                            -- -i
+        1 -> showGrammar $ transformGrammar grammar         -- -1
+        2 -> showFSM $ loadFSM $ transformGrammar grammar   -- -2
 
     -- putStr (show $ transformGrammar grammar)
     -- showFSM fsm
-    -- putStr (show $ nontermsToStates )
+    -- putStr (show $ grammar)
     -- putStr (show $ loadFSM (Grammar {nonterms = ["A","B"], terms = ["A","A1","A2","B"], rules = [("A",["a","A1"]),("A1",["a","B"]),("A",["c","A2"]),("A2",["c","B"]),("B",["b","B"]),("B",["#"])], initNonterm = "A"}))
