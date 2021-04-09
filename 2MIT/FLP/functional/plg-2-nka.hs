6aede96bfe9a554a5ef3b3c3dd7e5e75a2f5cd95
Izuwei <37940354+Izuwei@users.noreply.github.com>
2021-04-09 21:15:47 +0200

    Add files via upload

--- 2MIT/FLP/functional/plg-2-nka.hs
+++ 2MIT/FLP/functional/plg-2-nka.hs
@@ -1,6 +1,10 @@
 -- FIT VUT
--- Author: Jakub Sadilek
+
+-- PLG-2-NKA
+
+-- Autor: Jakub Sadilek
 -- Login: xsadil07
+-- Datum: 11.4.2021
 
 import System.Environment
 import System.IO
@@ -36,7 +40,7 @@ data FSM = FSM { states :: [State]              -- Q
                , finalStates :: [State]         -- F
                } deriving (Show, Eq)
 
--- Function returns pair (index, filename) according to program parameters.
+-- Funkce vrati dvojici (priznak, nazevSouboru) podle vstupnich param. programu.
 parseArgs :: Num a => [[Char]] -> (a, [Char])
 parseArgs args =
     case args of
@@ -48,73 +52,79 @@ parseArgs args =
         ["-2", file] -> (2, file)
         _ -> error "Unexpected program parameters."
 
--- Function loads content according to the format of file parameter.
+-- Funkce nacte obsah podle formatu parametru pro soubor.
 loadInput :: [Char] -> IO String
 loadInput file
-    | null file = getContents       -- No file, so get it from user
-    | otherwise = readFile file     -- Load from file
+    | null file = getContents       -- Nezadan soubor, cteme od uzivatele (stdin)
+    | otherwise = readFile file     -- nacteme ze souboru
 
--- Function splits string by given separator, then returns list of strings.
+-- Funkce rozdeli retezec podle zadaneho oddelovace, pote vrati list stringu.
 split :: Char -> [Char] -> [[Char]]
 split sep str =
     case break (==sep) str of
         (a, sep:b) -> a : split sep b
         (a, "") -> [a]
 
--- Function gets a list of strings (from file) and convert it into a grammar.
+-- Funkce dostane list stringu (radku ze souboru) a prevede ho na gramatiku.
 loadGrammar :: [[Char]] -> Grammar
 loadGrammar (nonterms : terms : initNonterm : rules) =
-    Grammar (checkNonterms (getSymbols nonterms))   -- Nonterms
-            (checkTerms (getSymbols terms))         -- Terms
-            (checkRules (getRules rules))           -- Rules
-            (checkNonterm initNonterm)              -- initial nonterm
+    Grammar (checkDuplicities (checkNonterms (getSymbols nonterms)))   -- Neterminaly
+            (checkDuplicities (checkTerms (getSymbols terms)))         -- Terminaly
+            (checkDuplicities (checkRules (getRules rules)))           -- Pravidla
+            (checkNonterm initNonterm)                                 -- Poc. neterminal
     where
         getSymbols line = split ',' line
 
-        -- Function checks if given symbol is in set of nonterminals.
+        -- Funkce zkontroluje, jestli zadany symbol patri do mnoziny neterminalu.
         checkNonterm symbol
             | symbol `elem` getSymbols nonterms = symbol
             | otherwise = error ("Symbol '" ++ symbol ++ "' is not nonterminal.")
 
-        -- Function get lines from input and parse them into rule struct (N, [N u T]).
+        -- Funkce dostane radky ze vstupu a kazdy prevede na strukturu pravidla (N, [N u T]).
         getRules lines = [splitRule line | line <- lines]
 
-        -- Function splits rule on arrow (->) and returns both sides in pair.
+        -- Funkce rozdeli pravidlo po znaku sipka (->) a vrati obe strany jako dvojici.
         splitRule rule@(x:y:z:xs)
             | [y, z] == "->" = ([x], map (: []) xs)
             | otherwise = error ("Unknown rule format: " ++ rule)
 
         splitRule rule = error ("Unknown rule format: " ++ rule)
 
-        -- Function checks if symbol is not empty. If is empty - error is raised.
-        isNotEmpty symbol
-            | null symbol = error "Empty symbols are not allowed."
-            | otherwise = symbol
+        -- Funkce zkontroluje, jestli symbol je dlouhy prave jeden znak.
+        isOne symbol
+            | length symbol == 1 = symbol
+            | otherwise = error ("Length of symbol '" ++ symbol ++ "' must be 1.")
 
-        -- Function checks if list of symbols is full of nonterms according to assignment.
-        checkNonterms symbols = [map isUppercase (isNotEmpty symbol) | symbol <- symbols]
+        -- Funkce zkontroluje, jestli list neobsahuje duplicity, protoze v mnozine nejsou povoleny.
+        checkDuplicities [] = []
+        checkDuplicities (x:xs)
+            | x `elem` xs = error "Duplicities are not allowed."
+            | otherwise = x : checkDuplicities xs
+
+        -- Funkce zkontruluje, jestli list neterminalu odpovida tvaru ze zadani.
+        checkNonterms symbols = [map isUppercase (isOne symbol) | symbol <- symbols]
             where
                 isUppercase symbol
                     | symbol `elem` ['A' .. 'Z'] = symbol
                     | otherwise = error ("Nonterminal '" ++ [symbol] ++ "' must be in range A-Z.")
 
-        -- Function checks if list of symbols is full of terms according to assignment.
-        checkTerms symbols = [map isLowercase (isNotEmpty symbol) | symbol <- symbols]
+        -- Funkce zkontruluje, jestli list terminalu odpovida tvaru ze zadani.
+        checkTerms symbols = [map isLowercase (isOne symbol) | symbol <- symbols]
             where
                 isLowercase symbol
                     | symbol `elem` ['a' .. 'z'] = symbol
                     | otherwise = error ("Terminal '" ++ [symbol] ++ "' must be in range a-z.")
 
-        -- Function checks if rules are in shape of right linear grammar.
+        -- Funkce zkontroluje, jestli pravidla jsou ve tvaru PLG.
         checkRules rls = [(checkNonterm (fst rule), checkRight (snd rule)) | rule <- rls]
             where
-                isAllTerm [] = True     -- Function checks if list of strings is full of terms, then returns bool.
+                isAllTerm [] = True     -- Kontrola, jestli list obsahuje pouze terminaly. Vraci bool.
                 isAllTerm [t] = True
                 isAllTerm (t:ts)
                     | t `notElem` getSymbols terms = False
                     | otherwise = isAllTerm ts
                 
-                -- Functions checks right side of the rule (xyz.., aB).
+                -- Funkce zkontroluje pravou stranu pravidla (xyz.., aB).
                 checkRight [] = error "Bad right side of rule."
                 checkRight nut =
                     if (last nut `elem` getSymbols nonterms && length nut /= 1)
@@ -131,7 +141,7 @@ loadGrammar (nonterms : terms : initNonterm : rules) =
 
 loadGrammar _ = error "Insufficient input."
 
--- Function prints grammar according the assignment.
+-- Funkce vypise gramatiku ve tvaru podle zadani.
 showGrammar :: Grammar -> IO ()
 showGrammar grammar = do
     putStrLn (intercalate "," (nonterms grammar))
@@ -139,17 +149,17 @@ showGrammar grammar = do
     putStrLn (initNonterm grammar)
     mapM_ (\rule -> putStrLn (fst rule ++ "->" ++ concat (snd rule))) (rules grammar)
 
--- Function gets starting number, nonterm and list of nonterms.
--- Then functions returns a new nonterm that is not in given list of nonterms.
+-- Funkce dostane pocatecni cislo, neterminal a list neterminalu.
+-- Pote vrati novy neterminal, ktery neni obsazen v zadanem list neterminalu.
 nontermGenerator :: Integer -> [Char] -> [[Char]] -> [Char]
 nontermGenerator n nonterm nonterms
     | (head nonterm : show n) `elem` nonterms = nontermGenerator (n+1) nonterm nonterms
     | otherwise = head nonterm : show n
 
--- Functions gets grammar rules and terms.
--- Then returns all nonterms, which are included in rules.
-getNonterms :: [[Char]] -> [([Char], [[Char]])] -> [[Char]]
-getNonterms terms rules = sort $ removeDuplicities $ getNontermList terms rules
+-- Funkce dostane pravidla gramatiky, neterminaly a terminaly.
+-- Pak vrati list vsech neterminalu, ktere jsou obsazeny v pravidlech + zadanem listu.
+getNonterms :: [[Char]] -> [[Char]] -> [([Char], [[Char]])] -> [[Char]]
+getNonterms nonterms terms rules = sort $ removeDuplicities $ nonterms ++ getNontermList terms rules
     where
         getNontermList terms [] = []
         getNontermList terms (r:rs) = [fst r] ++ filter (`notElem` (terms ++ ["#"])) (snd r) ++ getNontermList terms rs
@@ -158,93 +168,93 @@ getNonterms terms rules = sort $ removeDuplicities $ getNontermList terms rules
             | x `elem` xs = removeDuplicities xs
             | otherwise = x : removeDuplicities xs
 
--- Function transforms grammar according TIN algorithm in sentence 3.2.
+-- Funkce transformuje gramatiku podle TIN algoritmu ve vete 3.2.
 transformGrammar :: Grammar -> Grammar
 transformGrammar grammar =
-    Grammar (getNonterms (terms grammar) transformedRules)  -- Nonterms
-            (terms grammar)                                 -- Terms
-            transformedRules                                -- Rules
-            (initNonterm grammar)                           -- Initial nonterm
+    Grammar (getNonterms (nonterms grammar) (terms grammar) transformedRules)  -- Neterminaly
+            (terms grammar)                                 -- Terminaly
+            transformedRules                                -- Pravidla
+            (initNonterm grammar)                           -- Pocatecni neterminal
     where
-        -- New transformed rules
+        -- Nova transformovana pravidla
         transformedRules = transformRules (nonterms grammar) (rules grammar)
 
-        -- Transform long rule into sequence of short rules.
-        -- Creates a new rule and recursively do this for rest of the given rule.
+        -- Transformuje dlouha pravidla do sekvence kratkych pravidel.
+        -- Vytvori nove pravidlo a rerekurzivne opakuje proces pro zbytek daneho pravidla.
         cascadeRule nonterms (l, r) =
             (newNonterms, (l, [head r, newNonterm]) : newRules)
             where
-                newNonterm = nontermGenerator 1 l nonterms -- newNonterm
+                newNonterm = nontermGenerator 1 l nonterms -- novy neterminal
                 (newNonterms, newRules) = transformRule (nonterms ++ [newNonterm]) (newNonterm, tail r)
 
-        -- Function transforms simple terminal rule like X->a into X->aX1, X1->eps.
-        -- Returned in pair (new Nonterms, new Rules).
+        -- Transformuje ukoncujici pravidlo jako X->a na X->aX1, X1->eps.
+        -- Vrati dvojici (noveNeterminaly, novaPravidla).
         transformTerminalRule nonterms (l, r) =
             (nonterms ++ [newNonterm], [(l, [head r, newNonterm]), (newNonterm, ["#"])])
             where
                 newNonterm = nontermGenerator 1 l nonterms
 
-        -- Function transform a single rule.
-        -- Returns pair as (new Nonterms, new Rule).
+        -- Funkce transformuje jedno zadane pravidlo.
+        -- Vrati dvojici jako (noveNeterminaly, novaPravidla).
         transformRule nonterms rule@(l, r)
-            | length r == 1 && last r == "#" = (nonterms, [rule])   -- Terminal rule X->#
-            | length r == 1 = transformTerminalRule nonterms rule   -- Simple rule X->a. For not eps rules use this: (nonterms, [rule])
+            | length r == 1 && last r == "#" = (nonterms, [rule])   -- X->#
+            | length r == 1 = transformTerminalRule nonterms rule   -- X->a
             | length r == 2 && last r `elem` nonterms = (nonterms, [rule])  -- X->aB
-            | otherwise = cascadeRule nonterms rule     -- Other long rules like X->aaaa, X->aaaaB
+            | otherwise = cascadeRule nonterms rule     -- ostatni dlouha pravidla X->aaaa, X->aaaaB
 
-        -- Function gets list of rules and transform them, then new rules are returned.
-        transformRules _ [] = error "There is no rules."
+        -- Funkce dostane list pravidel, transformuje je a pak vrati nova pravidla.
+        transformRules _ [] = []
         transformRules nonterms [rule] = snd $ transformRule nonterms rule
         transformRules nonterms (rule:rules) =  newRules ++ transformRules newNonterms rules
             where
                 (newNonterms, newRules) = transformRule nonterms rule
 
--- Functions covenrts grammar into FSM.
+-- Funkce prevede gramatiku na konecny automat.
 loadFSM :: Grammar -> FSM
 loadFSM grammar =
-    FSM ([i | (_, i) <- convTable])                 -- states
-        (terms grammar)                             -- alphabet
-        (getTransitions (rules grammar))            -- transitions
-        (getState (initNonterm grammar) convTable)  -- initial state
-        (getFinalStates (rules grammar))            -- list of terminal states
+    FSM ([i | (_, i) <- convTable])                 -- stavy
+        (terms grammar)                             -- abeceda
+        (getTransitions (rules grammar))            -- prechody
+        (getState (initNonterm grammar) convTable)  -- poc. stav
+        (getFinalStates (rules grammar))            -- list ukoncujicich stavu
     where
-        convTable = nontermsToStates (nonterms grammar) -- Map nonterms into states
+        convTable = nontermsToStates (nonterms grammar) -- tabulka prevodu neterm. na stavy
 
-        -- Function gets list of nonterms and each nonterm is associated with number.
-        -- Numbers starts from 1 and increases with each nonterm. Retured as list of pairs.
+        -- Funkce dostane list neterminalu a kazdemu neterminalu priradi unikatni cislo.
+        -- Cislovani zacina od 1 a s kazdym neterm. se zvysuje. Vraci list dvojic.
         nontermsToStates = numberStates 1
             where
                 numberStates _ [] = []
                 numberStates n (x:xs) = (x, n) : numberStates (n+1) xs
 
-        -- Function gets nonterm and convTable and returns state, which corresponds to given nonterm.
+        -- Funcke dostane neterm. a prevadeci tabulku a vrati stav, ktery odpovida zadanemu neterm.
         getState nonterm [] = error ("Unknown Nonterm: " ++ nonterm)
         getState nonterm ((n, i):xs)
             | nonterm == n = i
             | otherwise = getState nonterm xs
 
-        -- Function convert single rule into transition.
+        -- Funkce prevede pravidlo gramatiky na prechod automatu.
         getTransition (l, r) = Transition (getState l convTable)
                                           (head r)
                                           (getState (last r) convTable)
 
-        -- Function converts list of rules into list of transitions.
+        -- Funkce prevede list pravidel na list prechodu.
         getTransitions [] = []
         getTransitions (rule:rules)
-            | length (snd rule) == 1 = getTransitions rules   -- skip terminal rules X->#
+            | length (snd rule) == 1 = getTransitions rules   -- preskoc ukoncujici pravidla X->#
             | otherwise = getTransition rule : getTransitions rules
 
-        -- Function returns all terminal states from grammar rules.
+        -- Funkce z gramatickych pravidel vrati vsechny ukoncujici stavy automatu.
         getFinalStates rules = map (\(a, _) -> getState a convTable) (filter (\(_, y) -> last y == "#") rules)
 
--- Function prints FSM according the assignment.
+-- Funkce vypise konecny automat ve tvaru podle zadani.
 showFSM :: FSM -> IO ()
 showFSM fsm = do
-    putStrLn (intercalate "," [show state | state <- states fsm])       -- states
-    putStrLn (concat (alphabet fsm))                                    -- alphabet
-    print (initState fsm)                                               -- initial state
-    putStrLn (intercalate "," [show state | state <- finalStates fsm])  -- final states
-    mapM_ putStrLn $ sort [intercalate "," [show (state t), symbol t, show (nextState t)] | t <- transitions fsm]   -- transitions
+    putStrLn (intercalate "," [show state | state <- states fsm])       -- stavy
+    putStrLn (concat (alphabet fsm))                                    -- abeceda
+    print (initState fsm)                                               -- poc. stav
+    putStrLn (intercalate "," [show state | state <- sort (finalStates fsm)])  -- konecne stavy
+    mapM_ putStrLn $ sort [intercalate "," [show (state t), symbol t, show (nextState t)] | t <- transitions fsm]   -- prechody
 
 main :: IO ()
 main = do
@@ -259,8 +269,3 @@ main = do
         0 -> showGrammar grammar                            -- -i
         1 -> showGrammar $ transformGrammar grammar         -- -1
         2 -> showFSM $ loadFSM $ transformGrammar grammar   -- -2
-
-    -- putStr (show $ transformGrammar grammar)
-    -- showFSM fsm
-    -- putStr (show $ grammar)
-    -- putStr (show $ loadFSM (Grammar {nonterms = ["A","B"], terms = ["A","A1","A2","B"], rules = [("A",["a","A1"]),("A1",["a","B"]),("A",["c","A2"]),("A2",["c","B"]),("B",["b","B"]),("B",["#"])], initNonterm = "A"}))
